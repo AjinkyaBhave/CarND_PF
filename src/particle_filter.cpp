ba1e9e1e9c7c574b1fe13835fb84fc44ee9416ac
AjinkyaBhave <ajinkyaybhave@gmail.com>
2017-12-01 19:54:38 +0530

    Implementing UpdateWeights().

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -25,25 +25,26 @@ void ParticleFilter::init(double x, double y, double theta, double std_pos[]) {
 	// Random number generator for Gaussian distribution
 	default_random_engine rand_gen;
 	// Gaussian distribution for x, y and theta
-	normal_distribution<double> dist_x(x, std_pos[0]);
-	normal_distribution<double> dist_y(y, std_pos[1]);
-	normal_distribution<double> dist_theta(theta, std_pos[2]);
+	normal_distribution<double> dist_x(0, std_pos[0]);
+	normal_distribution<double> dist_y(0, std_pos[1]);
+	normal_distribution<double> dist_theta(0, std_pos[2]);
 	// Temporary class to store particle initial data
 	Particle p;
 	// Initialize all particles to first position (based on estimates of 
 	// x, y, theta and their uncertainties from GPS) and all weights to 1. 
 	// Add random Gaussian noise to each particle.
+	// Particle positions are initialised in map coordinate frame.
 	for (int i=0; i<n_particles; i++){
-		p.x = dist_x(rand_gen);
-		p.y = dist_y(rand_gen);
-		p.theta = dist_theta(rand_gen);
+		p.x = x+dist_x(rand_gen);
+		p.y = y+dist_y(rand_gen);
+		p.theta = theta+dist_theta(rand_gen);
 		p.id = i;
 		p.weight = 1;
 		particles.push_back(p);
 	}
 	// Filter state is now initialised
 	is_initialized = true;
-	cout << "Particle number is " << particles.size() << endl; 
+	cout << "Filter initialised with " << particles.size() << " particles.\n"; 
 }
 
 void ParticleFilter::prediction(double delta_t, double std_pos[], double velocity, double yaw_rate) {
@@ -79,7 +80,7 @@ void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::ve
 
 void ParticleFilter::updateWeights(double sensor_range, double std_landmark[], 
 		const std::vector<LandmarkObs> &observations, const Map &map_landmarks) {
-	// TODO: Update the weights of each particle using a mult-variate Gaussian distribution. You can read
+	// TODO: Update the weights of each particle using a multi-variate Gaussian distribution. You can read
 	//   more about this distribution here: https://en.wikipedia.org/wiki/Multivariate_normal_distribution
 	// NOTE: The observations are given in the VEHICLE'S coordinate system. Your particles are located
 	//   according to the MAP'S coordinate system. You will need to transform between the two systems.
@@ -89,6 +90,34 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 	//   and the following is a good resource for the actual equation to implement (look at equation 
 	//   3.33
 	//   http://planning.cs.uiuc.edu/node99.html
+	std::vector<LandmarkObs> predicted;
+	LandmarkObs tempObs;
+	double min_obs_error;
+	double obs_error;
+	int id;
+	
+	for(int i=0; i<particles.size(); i++){
+		predicted.clear();
+		for(int j=0; j<observations.size();j++){
+			tempObs.x = particles[i].x + cos(particles[i].theta)*observations[i].x - sin(particles[i].theta)*observations[i].y;
+			tempObs.y = particles[i].y + sin(particles[i].theta)*observations[i].x + cos(particles[i].theta)*observations[i].y;
+			min_obs_error = sensor_range;
+			for(int k=0; k<map_landmarks.landmark_list.size(); k++){
+				obs_error = (map_landmarks.landmark_list[i].x_f-tempObs.x)*(map_landmarks.landmark_list[i].x_f-tempObs.x);
+				obs_error += (map_landmarks.landmark_list[i].y_f-tempObs.y)*(map_landmarks.landmark_list[i].y_f-tempObs.y);
+				obs_error = sqrt(obs_error);
+				if(obs_error < min_obs_error){
+					min_obs_error = obs_error;
+					id = map_landmarks.landmark_list[i].id_i;
+				}
+			}
+			tempObs.id = id;
+			predicted.push_back(tempObs);
+			
+		}
+		
+	}
+	
 }
 
 void ParticleFilter::resample() {
@@ -101,7 +130,7 @@ void ParticleFilter::resample() {
 Particle ParticleFilter::SetAssociations(Particle& particle, const std::vector<int>& associations, 
                                      const std::vector<double>& sense_x, const std::vector<double>& sense_y)
 {
-    //particle: the particle to assign each listed association, and association's (x,y) world coordinates mapping to
+    // particle: the particle to assign each listed association, and association's (x,y) world coordinates mapping to
     // associations: The landmark id that goes along with each listed association
     // sense_x: the associations x mapping already converted to world coordinates
     // sense_y: the associations y mapping already converted to world coordinates
